2cee9b5604073f22d04973ef8ef8de6307d85d39
Jasper Lankhorst <jasper_lankhorst80@hotmail.com>
2025-04-07 02:37:52 +0200

    add checks and test for day3.

--- adventofcode2024/adventofcode3/adventofcode3A/src/AdventOfCode3A.java
+++ adventofcode2024/adventofcode3/adventofcode3A/src/AdventOfCode3A.java
@@ -1,49 +1,139 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class AdventOfCode3A {
 
-    int digit1 = 0;
-    int digit2 = 0;
+    StringBuilder firstDigit = new StringBuilder();
+    StringBuilder secondDigit = new StringBuilder();
+    Integer result = 0;
 
     /**
      * Day 3: Mull It Over
      */
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
 
         AdventOfCode3A app = new AdventOfCode3A();
         FileInputStream fr = new FileInputStream(
                 "F:\\development\\adventofcode\\src\\adventofcode2024\\adventofcode3\\input.txt"
         );
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(fr));) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(fr))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] output = app.splitLineOnMulOpening(line);
-                for (String input : output) {
-                    if(app.isValidMultiplication(input)){
+
+                Pattern pattern = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+                Matcher matcher = pattern.matcher(line);
+                List<String> mulMatches = new ArrayList<>();
+
+                while (matcher.find()) {
+                    mulMatches.add(matcher.group());
+                }
+
+                for (String match : mulMatches) {
+                    System.out.println(match);
+
+                    char[] array = app.splitLineOnMulOpening(match.substring(3))[0].toCharArray();
+                    if (app.checkIsValidMultiplication(array)) {
+
+                        app.result += Math.multiplyExact(
+                                Integer.parseInt(app.firstDigit.toString()),
+                                Integer.parseInt(app.secondDigit.toString()));
+
+                        //numbers...
+//                        System.out.println(inputLine + " i:"+ i + " " +
+//                                "1d:"+ app.firstDigit.toString() + "," +
+//                                "2d:"+ app.secondDigit.toString() + "," +
+//                                "m:" + Math.multiplyExact(Integer.parseInt(app.firstDigit.toString()), Integer.parseInt(app.secondDigit.toString())) + "," +
+//                                "r:" + app.result);
 
                     }
                 }
             }
         }
+        System.out.println("Result : " + app.result);
+    }
+
+    public boolean checkIsValidMultiplication(char[] charArray) {
+
+        firstDigit = new StringBuilder();
+        secondDigit = new StringBuilder();
+
+        int firstChar = 0;
+        boolean findOpeningBracket = false;
+        boolean commaFound = false;
+        boolean findClosingBracket = false;
+
+        for (int i = 0; i < charArray.length; i++) {
+
+            // (
+            if (i == firstChar){
+                if (charArray[i] == '(') {
+                    findOpeningBracket = true;
+                    continue;
+                } else {
+                    System.out.println("Missing opening bracket");
+                    break;
+                }
+            }
+
+            //  (DDD,DDD)
+            //  0123,5678 max length 9
+            if (i <= 4 && !commaFound) {
+                if (!isDigit(charArray[i] + "") && charArray[i] != ',') {
+                    //invalid char found.
+                    firstDigit = new StringBuilder();
+                    secondDigit = new StringBuilder();
+                    System.out.println("Invalid char found in 1st digit");
+                    break;
+                }
+
+                if (isDigit(charArray[i] + "")) {
+                    firstDigit.append(charArray[i]);
+                    continue;
+                } else if (charArray[i] == ',' && firstDigit.length() > 0) {
+                    commaFound = true;
+                    continue;
+                }
+
+            } else if (!commaFound) {
+                //incorrect statement.
+                firstDigit = new StringBuilder();
+                secondDigit = new StringBuilder();
+                System.out.println("No comma found");
+                break;
+            }
+
+            //DDD) remaining. D length can be 1-3 but must have end bracket.
+            if (commaFound) {
+                if (isDigit(charArray[i] + "") && charArray[i] != ')' && secondDigit.length() < 3) {
+                    secondDigit.append(charArray[i]);
+                } else if (charArray[i] == ')') {
+                    findClosingBracket = true;
+                    break;
+                } else {
+                    //weird character found.
+                    System.out.println("weird char found in 2nd digit");
+                    firstDigit = new StringBuilder();
+                    secondDigit = new StringBuilder();
+                    break;
+                }
+            }else {
+                System.out.println("something weird happened");
+                break;
+            }
+        }
+        return findOpeningBracket && commaFound && findClosingBracket &&
+                firstDigit.length() <= 3 && firstDigit.length() > 0 &&
+                secondDigit.length() <= 3 && secondDigit.length() > 0;
     }
 
     public String[] splitLineOnMulOpening(String line) {
         return line.split("mul");
-    }
-
-    public boolean isValidMultiplication(String input) {
-        digit1 = 0;
-        digit2 = 0;
-
-        String openingChat = "(";
-        int digit1;
-        String isCommaFound = ",";
-        int digit2;
-        String endChar = ")";
-        return true;
     }
 
     public boolean isDigit(String inputString) {
--- adventofcode2024/adventofcode3/adventofcode3A/test/AdventOfCode3ATest.java
+++ adventofcode2024/adventofcode3/adventofcode3A/test/AdventOfCode3ATest.java
@@ -1,7 +1,6 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AdventOfCode3ATest {
 
@@ -15,20 +14,130 @@ class AdventOfCode3ATest {
     String input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     String inputSimpleRight = "mul(2,4)";
     String inputWrong = "mul[2,4]";
+    String inputbigNumber ="mul(233,239)";
 
     @Test
-    void testNumbersConsistentGoingDown_ExpectsTrue() {
-        AdventOfCode3A a = new AdventOfCode3A();
-
-        String[] k = a.getInputStatements(input);
-        assertEquals("2,4", k[0]);
-        assertEquals("5,5", k[1]);
-        assertEquals("11,8", k[2]);
-        assertEquals("8,5", k[3]);
+    void testMulReturnsProperString() {
+        Assertions.assertEquals(7, app.splitLineOnMulOpening(input).length);
     }
 
     @Test
-    void testIfStatementIsCorrect() {
+    void testSplitOnMulHasTwo() {
+        Assertions.assertEquals(2, app.splitLineOnMulOpening(inputSimpleRight).length);
+        Assertions.assertEquals(2, app.splitLineOnMulOpening(inputWrong).length);
+    }
 
+    @Test
+    void testSplitHasOpeningBracket() {
+        int rightSideOfSplit = 1;
+        int firstChar = 0;
+        Assertions.assertEquals('(', app.splitLineOnMulOpening(inputSimpleRight)[rightSideOfSplit].toCharArray()[firstChar]);
+        Assertions.assertEquals('[', app.splitLineOnMulOpening(inputWrong)[rightSideOfSplit].toCharArray()[firstChar]);
+    }
+
+    @Test
+    void loopThroughChars() {
+        int rightSideOfSplit = 1;
+        int firstChar = 0;
+        StringBuilder firstDigit = new StringBuilder();
+        StringBuilder secondDigit = new StringBuilder();
+        char[] charArray = app.splitLineOnMulOpening(inputSimpleRight)[rightSideOfSplit].toCharArray();
+
+        boolean findOpeningBracket = false;
+        boolean commaFound = false;
+        boolean findClosingBracket = false;
+        for (int i = 0; i < charArray.length; i++) {
+
+            // (
+            if (i == firstChar) {
+                findOpeningBracket = charArray[i] == '(';
+                continue;
+            }
+
+            // number until "," max size 3 if bigger as 3 numbers or different thing found incorrect.
+            if (findOpeningBracket && i <= 4 && !commaFound) {
+
+                if (!app.isDigit(charArray[i] + "") && charArray[i] != ',') {
+                    //invalid char found.
+                    break;
+                }
+
+                if (app.isDigit(charArray[i] + "")) {
+                    firstDigit.append(charArray[i]);
+                } else if (charArray[i] == ',' && firstDigit.length() > 0) {
+                    commaFound = true;
+                }
+
+            } else if (!commaFound) {
+                //incorrect statement.
+                break;
+            }
+
+            if (commaFound) {
+                if (app.isDigit(charArray[i] + "") && charArray[i] != ')' && secondDigit.length() < 3) {
+                    secondDigit.append(charArray[i]);
+                } else if (charArray[i] == ')') {
+                    findClosingBracket = true;
+                }
+            }
+        }
+
+        Assertions.assertEquals(firstDigit.toString(), "2");
+        Assertions.assertEquals(secondDigit.toString(), "4");
+        Assertions.assertTrue(findOpeningBracket);
+        Assertions.assertTrue(commaFound);
+        Assertions.assertTrue(findClosingBracket);
+    }
+
+    @Test
+    void testSimpleInputTextWithSingleMultiplication() {
+        String[] output = app.splitLineOnMulOpening(inputSimpleRight);
+        for (String inputLine : output) {
+            if (app.splitLineOnMulOpening(inputLine)[0].length() != 1) {
+                char[] array = app.splitLineOnMulOpening(inputLine)[0].toCharArray();
+                if (app.checkIsValidMultiplication(array)) {
+                    app.result = Math.multiplyExact(
+                            Integer.parseInt(app.firstDigit.toString()),
+                            Integer.parseInt(app.secondDigit.toString()));
+                }
+            }
+        }
+        Assertions.assertEquals(8, app.result);
+    }
+
+    @Test
+    void testSimpleInputTextWithMultiple_Multiplication_Counted() {
+        String[] output = app.splitLineOnMulOpening(input);
+        for (String inputLine : output) {
+            if (app.splitLineOnMulOpening(inputLine)[0].length() != 1) {
+                char[] array = app.splitLineOnMulOpening(inputLine)[0].toCharArray();
+                if (app.checkIsValidMultiplication(array)) {
+                    app.result += Math.multiplyExact(
+                            Integer.parseInt(app.firstDigit.toString()),
+                            Integer.parseInt(app.secondDigit.toString()));
+                }else{
+                    System.out.println(array);
+                }
+            }
+        }
+        Assertions.assertEquals(161, app.result);
+    }
+
+    @Test
+    void testBigNumberIsCorrectlyParsed_multiplicationTest(){
+        String[] output = app.splitLineOnMulOpening(inputbigNumber);
+        for (String inputLine : output) {
+            if (app.splitLineOnMulOpening(inputLine)[0].length() != 1) {
+                char[] array = app.splitLineOnMulOpening(inputLine)[0].toCharArray();
+                if (app.checkIsValidMultiplication(array)) {
+                    app.result += Math.multiplyExact(
+                            Integer.parseInt(app.firstDigit.toString()),
+                            Integer.parseInt(app.secondDigit.toString()));
+                }else{
+                    System.out.println(array);
+                }
+            }
+        }
+        Assertions.assertEquals(55687, app.result);
     }
 }
